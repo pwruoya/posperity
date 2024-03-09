aaafbf0ee7b3d8dff603766a2567801094c581d7
pwruoya <pruoya@kabarak.ac.ke>
2024-03-09 22:29:57 +0300

    home toggle menu

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="home.css">
+    <script src="https://kit.fontawesome.com/f7e75704ad.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -18,7 +19,16 @@
             }
             ?>
         </h1>
-        <nav class="nav">
+        <div class="menu">
+            <a onclick="toggleMenu()"><i class="fa-solid fa-bars"></i></a>
+            <div id="hide" class="navbar-toggle" >
+                <div class="bar">Home</div>
+                <div class="bar">About</div>
+                <div class="bar">Services</div>
+                <div class="bar">Services</div>
+            </div>
+        </div>
+        <nav class="nav" id="navbarLinks">
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="about.html">About</a></li>
@@ -31,24 +41,36 @@
 
     <div id="maindiv">
         <div class="main-content" id="div2">
-            <div class="main-content" id="div2">
-                <?php
-                if (isset($_SESSION['username'])) {
-                    // Display user details if logged in
-                    echo "<h2>Welcome, " . $_SESSION['username'] . "</h2>";
-                    echo "<div class='profile-image'><img src='assets\profile.png' alt='Profile Image'></div>";
-                } else {
-                    // Display login button if not logged in
-                    echo "<a href='login.php' class='button'>Login</a>";
-                }
-                ?>
-            </div>
+            <?php
+            if (isset($_SESSION['username'])) {
+                // Display user details if logged in
+                echo "<h2>Welcome, " . $_SESSION['username'] . "</h2>";
+                echo "<div class='profile-image'><img src='assets\profile.png' alt='Profile Image'></div>";
+            } else {
+                // Display login button if not logged in
+                echo "<a href='login.php' class='button'>Login</a>";
+            }
+            ?>
+            <button class="button">
+                make sale
+            </button>
         </div>
+
     </div>
     <footer>
         <p style="font-size: 10px;">
             &copy; 2024 posperity,all rights reserved</p>
     </footer>
 </body>
+<script>
+    function toggleMenu() {
+        var navbarLinks = document.getElementById("hide");
+        if (navbarLinks.style.display === "flex") {
+            navbarLinks.style.display = "none";
+        } else {
+            navbarLinks.style.display = "flex";
+        }
+    }
+</script>
 
 </html>
