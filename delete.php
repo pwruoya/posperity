caadc13094345563bbf524bd7b27ef5e7efd1b5f
ketertitus <ktitus@kabarak.ac.ke>
2024-03-28 09:27:52 +0300

    delete

---
+++ delete.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $_SESSION['selectedId'] = $_GET['product_id'];
+}
+
+include 'dbconfig.php';
+
+// Prepare and bind the SQL query with a placeholder for the ID
+$sql = "DELETE FROM `product` WHERE `product_id` = ?";
+$stmt = $conn->prepare($sql);
+
+// Bind the ID variable to the prepared statement
+$id = intval($_SESSION['selectedId']);
+$stmt->bind_param("i", $id);
+
+// Execute the statement
+$stmt->execute();
+
+// Check if the deletion was successful
+if ($stmt->affected_rows > 0) {
+    echo "Product deleted successfully.";
+} else {
+    echo "Failed to delete product.";
+}
+echo '<script>window.location.href = "inventory.php"</script>';
+
+$stmt->close();
+$conn->close();
--- inventory.php
+++ inventory.php
@@ -18,6 +18,12 @@ session_start();
     <script src="https://kit.fontawesome.com/f7e75704ad.js" crossorigin="anonymous"></script>
 </head>
 
+<style>
+    #icons {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
 
 <body>
     <header>
@@ -112,8 +118,8 @@ session_start();
                             // Otherwise, echo the quantity as normal
                             echo "<p>stock: $quantity</p>";
                         }
-                        echo '<div class="edit"><a href="editInventory.php?product_id=' . $prod . '"><i class="fa-regular fa-pen-to-square" style="color: #ffffff;"></i></a></div>';
-
+                        echo '<div id="icons"><div class="edit"><a href="editInventory.php?product_id=' . $prod . '"><i class="fa-regular fa-pen-to-square" style="color: #ffffff;"></i></a></div>';
+                        echo '<div class="edit"><a href="delete.php?product_id=' . $prod . '"><i class="fa fa-trash" aria-hidden="true" style="color: #ffffff;"></i></a></div></div>';
                         echo "</div>";
                         echo "</div>";
                     }
